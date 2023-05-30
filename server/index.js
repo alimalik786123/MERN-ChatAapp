8fc5c57fd29ff7433f66dbb18fae57a030672ab5
alimalik786123 <alimalik123010@gmail.com>
2023-05-30 22:32:39 +0530

    'fix'

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const dotenv=require('dotenv')
 const app=express()
 const mongo=require('./database/db')
 userRoutes=require('./Routes/userRoutes')
-
+ 
 mongo()
 // app.use(express.json)
 app.use('/',userRoutes)
