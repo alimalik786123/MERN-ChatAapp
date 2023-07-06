b9a6622e38c9be17986f2ee7abd65275b17899ca
alimalik786123 <alimalik123010@gmail.com>
2023-07-06 22:02:34 +0530

    json

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ userRoutes=require('./Routes/userRoutes')
   
 mongo()
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://mern-chat-aapp.vercel.app");
   res.header("Access-Control-Allow-Headers",
   "Origin, X-Requested-With, Content-Type, Accept"
   );
